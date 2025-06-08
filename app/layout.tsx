43da9e9ab3d1d857af6990ca78917754d978f7f3
SilverHawks <thekeke60@gmail.com>
2025-06-08 21:48:00 +0200

    add speed component from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/toaster"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           {children}
           <Analytics/>
+          <SpeedInsights/>
         </ThemeProvider>
         <Toaster />
       </body>
